af05981bd497c50c0d73ba1cbee0d4569fab79de
zhangwg <zhangwg@baihe.com>
2017-06-12 18:45:09 +0800

    添加发短信功能

---
+++ app-server/src/main/java/com/zh/dubbo/controller/CommonController.java
@@ -0,0 +1,101 @@
+package com.zh.dubbo.controller;
+
+import com.github.pagehelper.PageInfo;
+import com.zh.dubbo.constant.RspConstants;
+import com.zh.dubbo.entity.RespData;
+import com.zh.dubbo.entity.SysPermissionInit;
+import com.zh.dubbo.fo.SmsServiceFo;
+import com.zh.dubbo.untils.DateUtil;
+import com.zh.dubbo.untils.IPUtil;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by 70214 on 2017/4/23.
+ */
+@RestController
+public class CommonController {
+    @Autowired
+    SmsServiceFo smsServiceFo;
+
+    /**
+     * 用户未登录根据status前台跳转登录页面
+     * @param request
+     * @return
+     */
+    @GetMapping("/unlogin")
+    public RespData unlogin(HttpServletRequest request){
+       RespData respData = new RespData();
+       respData.setStatus(RspConstants.REQUESTERROR);
+       respData.setRspTime(DateUtil.getCurrentTime());
+       return respData;
+    }
+    @GetMapping("/login")
+    public String test1(HttpServletRequest request){
+        String result = "test1 this request ip +"+ IPUtil.getIpAddr(request);
+        System.out.println("IPUtil===="+result);
+        try {
+            UsernamePasswordToken token = new UsernamePasswordToken("18811328493", "112233");
+            SecurityUtils.getSubject().login(token);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return  "login failed";
+        }
+        return "login success";
+    }
+    @GetMapping("/unauthorized")
+    public List<SysPermissionInit> unauthorized(HttpServletRequest request){
+//        VCache.set("zhangsan","saaa");
+//        return userServiceFo.getAllSysPermissions();
+//        return "unauthorized";
+        return null;
+    }
+    @GetMapping("/test1")
+    public String login(HttpServletRequest request){
+        String result = "test1 this request ip +"+ IPUtil.getIpAddr(request);
+        System.out.println("IPUtil===="+result);
+        try {
+            UsernamePasswordToken token = new UsernamePasswordToken("13834412691", "a123123");
+            SecurityUtils.getSubject().login(token);
+        }catch (Exception e){
+            return  "login failed";
+        }
+        return "login success";
+    }
+    //踢出用户
+    @GetMapping(value="kickouting")
+    public String kickouting() {
+
+        return "kickouting";
+    }
+
+    //被踢出后跳转的页面
+    @GetMapping(value="kickout")
+    public String kickout() {
+        return "kickout";
+    }
+    @GetMapping("/sendSms")
+    public String sendSms(){
+        Map<String,Object> params = new HashMap<String,Object>();
+        params.put("mobile_phone","18811328493");
+        params.put("sms_template_id","SMS_52085322");
+        params.put("sms_template_nid","register");
+        Map<String,Object> templateParams = new HashMap<>();
+        templateParams.put("code","1314258");
+        params.put("template_params",templateParams);
+        return smsServiceFo.sendSms(params);
+    }
+
+    @GetMapping("/getSmsLogs")
+    public PageInfo getSmsLogs(){
+        Map<String,Object> params = new HashMap<String,Object>();
+        return smsServiceFo.getSendRecords(params);
+    }
+}
--- app-server/src/main/java/com/zh/dubbo/controller/MemberController.java
+++ app-server/src/main/java/com/zh/dubbo/controller/MemberController.java
@@ -1,11 +1,14 @@
 package com.zh.dubbo.controller;
 
 import com.zh.dubbo.constant.RspConstants;
+import com.zh.dubbo.core.shiro.mapper.manage.auth.AuthService;
 import com.zh.dubbo.core.shiro.tooken.manager.TokenManager;
 import com.zh.dubbo.entity.RespData;
 import com.zh.dubbo.entity.UUser;
 import com.zh.dubbo.fo.MemberServiceFo;
+import com.zh.dubbo.fo.SmsServiceFo;
 import com.zh.dubbo.untils.DateUtil;
+import com.zh.dubbo.untils.RandomUtil;
 import com.zh.dubbo.untils.RequestUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +26,50 @@ import java.util.Map;
 public class MemberController {
     @Autowired
     MemberServiceFo memberServiceFo;
+    @Autowired
+    SmsServiceFo smsServiceFo;
+    @Autowired
+    AuthService authService;
+    /**
+     * 注册获取验证码
+     * @param request
+     * @return
+     */
+    @GetMapping("getSmsCode")
+    public RespData sendSmsCode(HttpServletRequest request){
+        RespData respData = new RespData();
+        try {
+            Map<String, String> paramMap = RequestUtil.getQueryMap(request, "utf-8");
+            Map<String,Object> params = new HashMap<>();
+            params.putAll(paramMap);
+            if(paramMap == null || paramMap.size() == 0){
+                throw new Exception("参数列表不能为空！");
+            }
+            if(paramMap.get("mobile_phone") == null || "".equals(paramMap.get("mobile_phone").toString())){
+                throw new Exception("用户名不能为空!");
+            }
+            Boolean isPhone = authService.isPhoneAuth(params);
+            if(isPhone){
+                throw new Exception("手机号已被注册或绑定！");
+            }
+            params.put("sms_template_nid","register");
+            Map<String,Object> template_params = new HashMap<>();
+            String code = RandomUtil.sixCode();
+            //生成6位验证码并存入redis中
+            template_params.put("code",code);
+            params.put("template_params",template_params);
+            smsServiceFo.sendSms(params);
+            respData.setRspTime(DateUtil.getCurrentTime());
+            respData.setStatus(RspConstants.SUCCESS);
+            respData.setMessage("短信发送成功！");
+            return respData;
+        }catch (Exception e){
+            respData.setMessage("短信发送失败");
+            respData.setStatus(RspConstants.OTHERERROR);
+            respData.setRspTime(DateUtil.getCurrentTime());
+            return respData;
+        }
+    }
     /**
      * 忘记密码
      * @param request
@@ -58,4 +105,5 @@ public class MemberController {
             return respData;
         }
     }
+
 }
