88190af089d5c126b06553f930f8f0495d54c2fd
zhangwg <zhangwg@baihe.com>
2017-06-18 09:26:51 +0800

    增加跨域

---
+++ app-server/src/main/java/com/zh/dubbo/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.zh.dubbo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Administrator on 2017/6/16.
+ */
+@Configuration
+public class CorsConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST","PUT", "DELETE", "OPTIONS")
+                .allowCredentials(false).maxAge(3600);
+    }
+}
